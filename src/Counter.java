65ed2b0eb9e3aa455d1e5016ea0cc7e4c4ac808b
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-06-07 15:16:01 +0300

    Add an increment method

---
+++ src/Counter.java
@@ -0,0 +1,9 @@
+public class Counter {
+    int count;
+
+    public  void  increment(){
+        count++;
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,12 @@
 public class Main {
     public static void main(String[] args) {
 
+        Counter c = new Counter();
+        c.increment();
+
+        System.out.println(c.count);
+
+
+
     }
 }
